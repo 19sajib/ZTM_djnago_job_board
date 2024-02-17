116003551813e053a1f8e31c2b759795e84b87e6
Md Abu Bakkar Siddiqe Sajib <contact2sajib@gmail.com>
2024-02-17 19:05:25 +0600

    db migration & data call

--- job_board/views.py
+++ job_board/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import JobPosting
+
 def index(request):
-    return HttpResponse('<h1>Job board</h1>')
+    # jobs = JobPosting.objects.all()
+    acitve_posting = JobPosting.objects.filter(is_active=True)
+    context = {
+        'job_postings': acitve_posting
+    }
+    return render(request, 'job_board/index.html' , context)
